403e1b5726e7b15c80a65415daea0ba45bc27aa8
Anton Netudykhata <anton.netudykhata@uptech.team>
2017-05-06 12:29:30 +0300

    Implementing add comment

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -10,15 +10,36 @@ class AddComment extends Component {
   // componentWillMount(nextProps) {
   //   this.props.CollectionStore.getComments()
   // }
+  constructor(props) {
+    super(props)
+    this.state = {
+     comment: ''
+    }
+    this.onSend = this.onSend.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  onSend() {
+    console.log('sand clicked')
+    console.log(this.state.comment)
+  }
+
+  handleChange(e) {
+    this.setState({comment: e.target.value});
+  }
 
   render() {
     return (
 
       <div className="add-comment-wrapper">
         <div className="add-comment">
-          <button className="add-comment-btn">Send</button>
+          <button className="add-comment-btn" onClick={this.onSend} >Send</button>
           {/* TODO <RaisedButton label="Primary" primary={true} />*/}
-          <div className="add-comment-area-wrapper" ><textarea className="add-comment-area" /></div>
+          <div className="add-comment-area-wrapper">
+            <textarea className="add-comment-area" 
+                      onChange={this.handleChange}
+            />
+          </div>
         </div>
       </div>);
   }
