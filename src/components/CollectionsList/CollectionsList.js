4f87b1ae174c3bd74d9314acf609e63bd0cb03d0
Alexei <dewstead@dews-MacBook-Air.local>
2017-05-10 00:16:46 +0300

    *Fixed voting; *added avatar and more info to collection card on home page; *fixed bug with redirecting to end of the flow when last question is voted in SurveyFlow.js

--- src/components/CollectionsList/CollectionsList.js
+++ src/components/CollectionsList/CollectionsList.js
@@ -4,14 +4,17 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { observer, inject } from "mobx-react";
 import { Link } from 'react-router-dom';
 import CollectionSearch from '../CollectionSearch';
+import smallLogo from './represent_white_outline.svg';
+
+import './CollectionsList.css';
 
 const styles = {
-  cardStyle: {
-    margin: '10px',
-    maxWidth: 900
+  containerStyle: {
+    padding: 10
   },
-  imgStyle: {
-    maxWidth: 900
+  cardStyle: {
+    maxWidth: 400,
+    margin: '10px auto'
   }
 }
 
@@ -26,25 +29,37 @@ const CollectionsList = inject("CollectionStore")(observer(({ CollectionStore })
   return (
     <div>
       <div><CollectionSearch /></div>
+      <div style={styles.containerStyle}>
       {collections.map((collection_obj) => {
         const id = collection_obj[0];
         const collection = collection_obj[1];
+        const first_name = collection.user.first_name ? collection.user.first_name : '';
+        const last_name = collection.user.last_name ? collection.user.last_name : '';
+        const user_name = `${first_name} ${last_name}`;
+        const bio = collection.user.bio ? collection.user.bio : '';
+        const location = collection.user.country_info ? collection.user.country_info.name : '';
+        const photo = collection.user.photo ? collection.user.photo.replace("localhost:8000", "represent.me") : smallLogo;
+        const image = collection.photo ? collection.photo.replace("localhost:8000", "represent.me") : null;
+        //const link = "https://app.represent.me/profile/" + collection.user.id + "/" + collection.user.username; //our user
+
+
         return (
           <Card style={styles.cardStyle} key={ id }>
-            {/* <CardTitle
-              title={ collection.name }
-            /> */}
-            <CardMedia
-              overlay={<CardTitle
-                title={collection.name}
-                />}
-            >
-              <img src={collection.photo.replace("localhost:8000", "represent.me")}
-              style={styles.imgStyle}/>
-
+            <CardHeader
+                title={user_name}
+                subtitle={`${bio} ${location}`}
+                avatar={photo}
+                className='cardHeaderStyle'
+            />
+            <CardMedia>
+              <img src={image} className='imgStyle'/>
             </CardMedia>
+            <CardTitle
+              title={ collection.name }
+            />
             <CardText>
-              { collection.desc }
+              {collection.desc.slice(0, 200 + collection.desc.indexOf(' ')) + ' '}
+              <Link to={ "/survey/" + id }><i>more...</i></Link>
             </CardText>
             <CardActions>
               <Link to={ "/survey/" + id }>
@@ -52,8 +67,9 @@ const CollectionsList = inject("CollectionStore")(observer(({ CollectionStore })
               </Link>
             </CardActions>
           </Card>
-        )
+          )
       })}
+      </div>
     </div>
   );
 }))
