875a67087476b94ea3c5065bfb49b82ed01e57e5
Ed Dowding <eddowding@gmail.com>
2017-05-15 03:13:19 +0100

    branding load indicator

--- src/components/LoadingIndicator/LoadingIndicator.jsx
+++ src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { inject, observer } from "mobx-react";
-import { observable, reaction } from "mobx";
-import CircularProgress from 'material-ui/CircularProgress';
-import { cyan600 } from 'material-ui/styles/colors';
+import { observable, reaction } from "mobx"; 
 
 let containerStyle = {
-    'width': '100%',
-    'height': '100%',
+    'width': '100vw',
+    'height': '100vh',
     'display': 'flex',
     'justifyContent': 'center',
     'alignItems': 'center',
-    'backgroundColor': '#eeeeee'
+    'backgroundColor': '#fff',
+    'color' : '#1B8AAE',
 }
 
 const LoadingIndicator = (props) => {
     let text = props.text || "Loading...";
     return (
-        <div style={containerStyle}><CircularProgress size={80} thickness={5} color={cyan600} /></div>
+        <div style={containerStyle}> 
+            <img src="https://app.represent.me/assets/img/logos/animated/long-100.gif" />
+        </div>
+        
     )
 }
 
