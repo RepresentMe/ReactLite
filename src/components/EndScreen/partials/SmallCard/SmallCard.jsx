d49e328f805747c2ba35ac2e51d240d70d5b5370
Ed Dowding <eddowding@gmail.com>
2017-05-15 13:26:53 +0100

    Real values in results
    also placeholder for ‘say why’ action
    
     {/*  <CardText>
        You are in the majority. [OR IF AGREE < 50%]
          You are in the minority. Say why.
        </CardText> */}

--- src/components/EndScreen/partials/SmallCard/SmallCard.jsx
+++ src/components/EndScreen/partials/SmallCard/SmallCard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { observer } from "mobx-react";
 import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
+import Divider from 'material-ui/Divider';
 
 @observer
 class SmallCard extends Component {
@@ -12,8 +12,8 @@ class SmallCard extends Component {
       <div>
         {!this.props.data && <p></p>}
         {this.props.data.values &&
-          <div style={{minHeight: 500}}>
-            <Card style={{minHeight: 300}}>
+          <div style={{minHeight: 0}}>
+            <Card style={{minHeight: 0}}>
          {/*      <div style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'spaceBetween'}}>
                 <div style={{flex: 1, margin: 15,  width: 200, height: 250, border: '2px solid black'}}>
                   <div style={{flex: 1, borderBottom: '2px solid grey', color: 'white', padding: 10, fontWeight: 'bold', backgroundColor: this.props.data.values[0].fill, minHeight: 70}}>
@@ -31,15 +31,19 @@ class SmallCard extends Component {
           */}
 
 
-
-
-    <CardTitle title="47%" subtitle="Agree with you" style={{backgroundColor: this.props.data.values[0].fill, color: 'white'}} />
+ 
+  {/*  <CardText>
+    You are in the majority. [OR IF AGREE < 50%]
+      You are in the minority. Say why. 
+    </CardText> */}
+    <CardTitle title={`${this.props.data.values[0].percentage}%`} subtitle="Agree with you" style={{backgroundColor: this.props.data.values[0].fill, color: 'white'}} />
     <CardText style={{fontSize: '20px', fontWeight: '200'}}>
       {this.props.data.values[0].title}
     </CardText>
     <Divider />
     <CardActions>
-      <FlatButton label="Details" /> 
+      <FlatButton label="Details" primary={true}/> 
+      <FlatButton label="Share" /> 
     </CardActions>
    
           </Card>
