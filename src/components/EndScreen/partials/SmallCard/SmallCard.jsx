df896c1493df6d0db1fbf1a6709609a5b8840f1c
Alexei <dewstead@dews-MacBook-Air.local>
2017-05-21 12:46:09 +0300

    Added comments icon instead of comments label for small results card on EndScreen

--- src/components/EndScreen/partials/SmallCard/SmallCard.jsx
+++ src/components/EndScreen/partials/SmallCard/SmallCard.jsx
@@ -8,6 +8,7 @@ import ChangeAnswer from 'material-ui/svg-icons/content/undo';
 import SocialShare from 'material-ui/svg-icons/social/share';
 import ChartIcon from 'material-ui/svg-icons/editor/insert-chart';
 import IconButton from 'material-ui/IconButton';
+import InsertComment from 'material-ui/svg-icons/editor/insert-comment'
 
 import QuestionLiquidPiechart from '../../../charts/QuestionLiquidPiechart';
 
@@ -48,34 +49,60 @@ class SmallCard extends Component {
                     {this.props.data.values[0].full_name ? <QuestionLiquidPiechart questionId={this.props.data.values[0].questionId} pie={false} endScreen={true}/> : ""}
                   </div>
 
-                 
+
                     {this.props.data.values[0].full_name ? <span>{}</span> :
                      <div style={{color: 'white', fontWeight: 'bold', width: 220, borderBottom: '1px solid #ccc', backgroundColor: this.props.data.values[0].fill, minHeight: 60, padding: 10}}>
                      <p className='didnotanswer'>No vote. <span className='linkit' onTouchTap={(e) => this.redirect(e, 'vote')}>Answer now?</span></p></div>}
-                     
-{/* 
+
+{/*
                      <p style={{color: 'white', fontWeight: 'bold', fontSize: 35, margin: 0, textAlign: 'left'}}>{`${this.props.data.values[0].percentage}%`}</p>
                     {this.props.data.values[0].full_name ? <p style={{color: 'white', fontSize: 14, textAlign: 'left',  margin: 0, fontWeight: '200', opacity: '0.8'}}>{`${this.props.data.values[0].full_name}`}</p> : ''}
 */}
 
 
-                  <div style={{ width: 240, backgroundColor: "#f5f5fe"}}>
+                  <div style={{ width: 240, backgroundColor: "#f5f5fe", height: 36}}>
                     <div style={{position: 'relative'}}>
-                      <FlatButton label='comments' primary={true} />
+                      <IconButton
+                        tooltip="comment"
+                        touch={true}
+                        tooltipPosition="top-center"
+                        onTouchTap={(e) => this.redirect(e, 'comments')}
+                        iconStyle={{height: 16}}
+                        style={{position: 'absolute', left: 0, top: -5}}>
+                        <InsertComment style={iconStyles} color='#999' hoverColor='#1B8AAE' />
+                      </IconButton>
                       <div style={{position: 'absolute',
-                        top: 10, left: 100, fontSize: 10, borderRadius: '50%',
+                        top: 12, left: 40, fontSize: 8, borderRadius: '50%',
                         border: `1px solid ${cyan400}`, backgroundColor: cyan200,
-                        color: 'white', width: 12, height: 12, textAlign: 'center'}}
+                        color: 'white', width: 10, height: 10, paddingTop: 1, textAlign: 'center'}}
                         >
                           {this.props.data.values[0].count_comments}
                       </div>
-                      <IconButton tooltip="change answer" touch={true} tooltipPosition="top-center" onTouchTap={(e) => this.redirect(e, 'vote')} style={{position: 'absolute', right: 70, top: -5}}>
-                        <ChangeAnswer style={iconStyles} color='#999' hoverColor='#1B8AAE' />
+                      <IconButton
+                        tooltip="change answer"
+                        touch={true}
+                        tooltipPosition="top-center"
+                        onTouchTap={(e) => this.redirect(e, 'vote')}
+                        iconStyle={{height: 16}}
+                        style={{position: 'absolute', right: 65, top: -5}}>
+                        <ChangeAnswer style={iconStyles} color='#999' hoverColor='#1B8AAE' style={{height: 16}} />
                       </IconButton>
-                      <IconButton tooltip="share" touch={true} tooltipPosition="top-center" onTouchTap={(e) => this.redirect(e, 'share')} style={{position: 'absolute', right: 40, top: -5}}>
+                      <IconButton
+                        tooltip="share"
+                        touch={true}
+                        tooltipPosition="top-center"
+                        onTouchTap={(e) => this.redirect(e, 'share')}
+                        iconStyle={{height: 16}}
+                        style={{position: 'absolute', right: 35, top: -5}}>
                         <SocialShare style={iconStyles} color='#999' hoverColor='#1B8AAE' />
                       </IconButton>
-                      <IconButton tooltip="results" touch={true} tooltipPosition="top-center" onTouchTap={(e) => this.redirect(e, 'results')} style={{position: 'absolute', right: 5, top: -5}}>
+                      <IconButton
+                        tooltip="results"
+                        touch={true}
+                        tooltipPosition="top-center"
+                        iconStyle={{height: 16}}
+                        onTouchTap={(e) => this.redirect(e, 'results')}
+                        style={{position: 'absolute', right: 0, top: -5}}>
                         <ChartIcon style={iconStyles} color='#999' hoverColor='#1B8AAE' />
                       </IconButton>
                     </div>
