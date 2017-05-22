8ae0ae3df5929a1f478e85ec24eb8498885c3e2b
Alexei <dewstead@dews-MacBook-Air.local>
2017-05-22 13:11:09 +0300

    CollectionIntro screen will show 200 chars text with possibility to expand to full collection description; Modified MoreText component so that number of chars to truncate text can be passed via prop and option to display text via Reactmarkdown is also defined via prop

--- src/components/Components/MoreText/MoreText.jsx
+++ src/components/Components/MoreText/MoreText.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const COMMENT_MAX_SIZE_TO_SHOW = 140;
 
@@ -13,7 +14,10 @@ class MoreText extends Component {
   }
 
   componentWillMount() {
-    if (this.props.text.length > 140) {
+    if (!this.props.chars && this.props.text.length > 140) {
+      this.setState({ shouldBeTruncated: true })
+    }
+    else if (this.props.chars && this.props.text.length > 200) {
       this.setState({ shouldBeTruncated: true })
     } else {
       this.setState({ readMore: true })
@@ -29,24 +33,33 @@ class MoreText extends Component {
     const { shouldBeTruncated, readMore } = this.state;
 
     let truncatedComment;
-    if (shouldBeTruncated) {
+    if (shouldBeTruncated && !this.props.chars) {
       truncatedComment = (shouldBeTruncated) ? text.substring(0, COMMENT_MAX_SIZE_TO_SHOW) : text
+    } else if (shouldBeTruncated && this.props.chars) {
+      truncatedComment = (shouldBeTruncated) ? text.substring(0, this.props.chars) : text
     }
     const buttonStyle={
       cursor: 'pointer'
     }
-
+    console.log('truncatedComment', truncatedComment, this.props)
     return (
       <div className="moreText">
-        {
+        {this.props.markdownEnabled ?
           readMore ? (
-            <p>{text}</p>
-          ) : (
-              <p> {`${truncatedComment}...`}
+            <ReactMarkdown source={ text } className="markDownText" renderers={{Link: props => <a href={props.href} target="_blank">{props.children}</a>}} />
+            ) : (
+              <div>
+                <ReactMarkdown source={ truncatedComment } className="markDownText" renderers={{Link: props => <a href={props.href} target="_blank">{props.children}</a>}} />
                 <a style={buttonStyle} onClick={this.readMoreClick}> Read more</a>
-              </p>
-
-            )
+              </div>
+            ) :
+            readMore ? (
+              <p>{text}</p>
+            ) : (
+                <p> {`${truncatedComment}...`}
+                  <a style={buttonStyle} onClick={this.readMoreClick}> Read more</a>
+                </p>
+              )
         }
       </div>
     )
