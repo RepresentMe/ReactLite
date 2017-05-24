23a95470a2f6c12c7cd5f72ea2c9b72d8caf05a9
Андрей Михацкий <andrey.mikhatskiy@gmail.com>
2017-05-25 01:16:10 +0300

    refactored EditCollection

--- src/components/EditCollection/EditCollection.jsx
+++ src/components/EditCollection/EditCollection.jsx
@@ -11,9 +11,10 @@ import LinearProgress from 'material-ui/LinearProgress';
 
 @inject("QuestionStore", "CollectionStore", "UserStore") @observer class EditCollection extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+    this.collectionId = props.match.params.collectionId;
     this.state = {
       title: "",
       description: "",
@@ -24,7 +25,6 @@ import LinearProgress from 'material-ui/LinearProgress';
     }
 
     this.fillDetailsFromStore = this.fillDetailsFromStore.bind(this);
-    this.checkForUpdates = this.checkForUpdates.bind(this);
   }
 
   fillDetailsFromStore() {
@@ -38,66 +38,55 @@ import LinearProgress from 'material-ui/LinearProgress';
     });
   }
 
-  checkForUpdates() {
-    console.log('checkForUpdates COMPONENT')
-    let collectionId = parseInt(this.props.match.params.collectionId);
-
-    if(this.props.CollectionStore.collections.has(collectionId) && !this.state.hasCollectionDetails) { // Are the collection details already cached?
-      console.log('checkForUpdates IF')
-      this.fillDetailsFromStore();
-
-    }else {
-      console.log('checkForUpdates ELSE')
-      this.props.CollectionStore.getCollection(collectionId);
-    }
-
-    if(this.props.QuestionStore.questions.has(collectionId) && !this.state.hasCollectionQuestions) { // Are the collection questions already cached?
-      const questions = this.props.CollectionStore.collectionItems.get(collectionId);
-      this.setState({ questions, hasCollectionQuestions: true });
-    }else {
-      this.props.CollectionStore.getCollectionItemsById(collectionId);
-      const questions = this.props.CollectionStore.collectionItems.get(collectionId);
-      this.setState({ questions, hasCollectionQuestions: true });
-    }
-  }
 
   componentWillMount() {
-    this.checkForUpdates();
-  }
+    // this.fillDetailsFromStore();
+    let collectionId = parseInt(this.props.match.params.collectionId);
+    this.props.CollectionStore.getCollectionItemsById(collectionId).then((items) => {
+      const questions = this.props.CollectionStore.collectionItems.get(collectionId);
+      this.setState({ questions, hasCollectionQuestions: true });
+      console.log('will mount', questions);
 
-  componentWillReact() { // Called every time the store updates (Requires a reference to store in render())
-    let collectionId = parseInt(this.props.match.params.collectionId); // Check if user is the owner of the collection, otherwise navigate away
-    // if(this.props.UserStore.userData.has("id") && this.props.CollectionStore.collections.get(collectionId).user.id !== this.props.UserStore.userData.get("id")) {
-    //   this.props.push("/");
-    // }
-    this.checkForUpdates();
+    });
   }
+  
   addItem = (collectionId, questionId, type) => {
     this.props.CollectionStore.setCollectionQuestionById(collectionId, questionId, type)
-    .then(res => this.checkForUpdates());
     
+  }
+
+  saveItems = () => {
+    let curItems = this.props.CollectionStore.collectionItems.get(this.collectionId);
+    let newItems = [];
+    this.state.questions.forEach((question, j) => {
+      for (let i = 0; i < curItems.length; i++) {
+        if(curItems[i].object_id == question.id) {
+          curItems.order = j;
+          newItems.push(curItems);
+          break;
+        }
+      }
+    })
   }
 
   render() {
     let collectionId = parseInt(this.props.match.params.collectionId);
 
-    console.log(!this.props.CollectionStore.collections.has(collectionId), !this.props.CollectionStore.collectionItems.has(collectionId), !this.props.UserStore.userData.has("id"))
-
     let questions = null;
     let question_objects = null;
-    if(!this.props.CollectionStore.collections.has(collectionId) || !this.props.CollectionStore.collectionItems.has(collectionId) || !this.props.UserStore.userData.has("id")) { //!this.props.QuestionStore.questions.has(collectionId) ||
+    console.log('RENDER', this.state.questions, this.state.questions.length);
+    if(!this.state.questions || this.state.questions.length == 0) { //!this.props.QuestionStore.questions.has(collectionId) ||
       return <LinearProgress mode="indeterminate" />;
-    }
-    else {
+    } else {
 
       this.props.CollectionStore.getCollectionItemsById(collectionId);
-      questions = this.props.CollectionStore.collectionItems.get(collectionId);
+      questions = this.state.questions;//this.props.CollectionStore.collectionItems.get(collectionId);
       question_objects = questions.map(q => this.props.QuestionStore.questions.get(q.object_id))
     }
     console.log('this.state EDIT',this.state, question_objects)
     return (
       <div>
-        {this.state.questions.length > 0 &&
+        {this.state.questions && this.state.questions.length > 0 &&
         <div>
         <CollectionAdminGUI
           title={this.state.title}
@@ -121,6 +110,7 @@ import LinearProgress from 'material-ui/LinearProgress';
           }}
 
           sortQuestion={(oldIndex, newIndex) => {
+            console.log('sortQuestion: ', oldIndex, newIndex);
             this.setState({questions: arrayMove(questions, oldIndex, newIndex)});
           }}
           />
