3319ec7209aca571f3c97b8ac1f73e3216959d92
Андрей Михацкий <andrey.mikhatskiy@gmail.com>
2017-05-26 14:50:58 +0300

    fixed logout not working

--- src/Stores/UserStore.js
+++ src/Stores/UserStore.js
@@ -90,7 +90,7 @@ class UserStore {
   setupAuthToken(authToken) {
     return new Promise((resolve, reject) => {
       this.sessionData.set("authToken", authToken);
-      Cookies.set("representAuthToken", authToken, { expires: Infinity, domain: window.location.origin =="http://localhost:3000" ? null : 'represent.me' });
+      Cookies.set("representAuthToken", authToken, { expires: Infinity, domain: (window.location.origin.indexOf("http://localhost") != -1) ? null : 'represent.me' });
       window.API.defaults.headers.common['Authorization'] = "Token " + authToken;
       this.getMe()
         .then((response) => {
@@ -171,7 +171,7 @@ class UserStore {
 
 
   logout() {
-    Cookies.expire("representAuthToken");
+    Cookies.expire("representAuthToken", { path: (window.location.origin.indexOf("http://localhost") != -1) ? null : 'represent.me' });
     this.sessionData.set("authToken", "");
     this.userData.replace({});
     this.sessionData.set("showUserDialogue", false);
